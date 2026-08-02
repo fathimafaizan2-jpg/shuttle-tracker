42e4222800044d30afe2b175f9172d592c1def1a
fathimafaizan2-jpg <fathimafaizan2@gmail.com>
2026-08-02 21:38:44 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "shuttle-tracker-v1";
+const urlsToCache = ["./index.html", "./manifest.json"];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch Cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
