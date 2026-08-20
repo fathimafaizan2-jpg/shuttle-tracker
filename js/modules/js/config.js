832f28caa17b6356801fda194cccc487901709de
fathimafaizan2-jpg <fathimafaizan2@gmail.com>
2026-08-20 11:31:42 +0300

    Create config.js for data and state management
    Added initial data and state configuration for flights and schedule.

---
+++ js/modules/js/config.js
@@ -0,0 +1,17 @@
+// DATA & STATE CONFIGURATION
+const state = { 
+  role: "PLAYER", 
+  page: "home", 
+  attendance: "COMING" 
+};
+
+const flights = ["Premier", "Flight 1", "Flight 2", "Flight 3", "Flight 4A", "Flight 4B"];
+
+const schedule = [
+  ["Saturday",  "Flight 2", "Flight 1", "Premier",  "Premier"],
+  ["Sunday",    "Flight 4A", "Flight 4B", "Flight 3", "Flight 3"],
+  ["Monday",    "Flight 1", "Flight 2", "Premier",  "Premier"],
+  ["Tuesday",   "Flight 3", "Flight 3", "Flight 4A", "Flight 4B"],
+  ["Wednesday", "Premier", "Premier", "Flight 2", "Flight 1"],
+  ["Thursday",  "Flight 4A", "Flight 4B", "Flight 3", "Flight 3"]
+];
