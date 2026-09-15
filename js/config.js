ecfc118218c0348a76aa620fa5a50a0ac5a50463
fathimafaizan2-jpg <fathimafaizan2@gmail.com>
2026-09-15 17:31:53 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
@@ -14,8 +13,6 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
 
-/* Browser-local persistence keeps the account signed in through refreshes and normal browser restarts. */
 await setPersistence(firebaseAuth, browserLocalPersistence);
 
 export { firebaseApp, firebaseAuth };
-
